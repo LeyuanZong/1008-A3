8e70c5c9f509273a0873dfa601dcaec418e2aa2c
LemonBiscuit1 <sghoyee@gmail.com>
2023-05-27 03:32:37 +1000

    task 4 progress?

--- balancing.py
+++ balancing.py
@@ -1,13 +1,23 @@
 from __future__ import annotations
 from threedeebeetree import Point
+from ratio import Percentiles
 
 def make_ordering(my_coordinate_list: list[Point]) -> list[Point]:
+    ordered_list = []
+    x_calculator = Percentiles()
+    y_calculator = Percentiles()
+    z_calculator = Percentiles()
+
+    for point in my_coordinate_list:
+        x_calculator.add_point(point[0])
+        y_calculator.add_point(point[1])
+        z_calculator.add_point(point[2])
+
+    rootX = x_calculator.ratio(0,0)
+    rootY = y_calculator.ratio(0,0)
+    rootZ = z_calculator.ratio(0,0)
     
     
-    
-    
-    
-    
-    
-    
-    raise NotImplementedError()
+    ordered_list = my_coordinate_list
+
+    return ordered_list
