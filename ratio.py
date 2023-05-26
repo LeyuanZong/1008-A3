64a4e4aed6a97a8f3f05eb23da0fb9aff932a3bc
LeyuanZong <lzon0002@student.monash.edu>
2023-05-27 01:44:16 +1000

    Add complexity to task1

--- ratio.py
+++ ratio.py
@@ -9,15 +9,39 @@ I = TypeVar("I")
 class Percentiles(Generic[T]):
 
     def __init__(self) -> None:
+
+        # complexity: O(1)
+
         self.tree = BinarySearchTree()
     
     def add_point(self, item: T):
+        """
+        complexity best: O(CompK) inserts the item at the root.
+        complexity worst: O(Compk * D) inserting at the bottom of the tree
+        where D is the depth of the tree
+        CompK is the complexity of comparing the keys
+        Refer to __setitem__() in bst.BinarySearchTree
+        """
+
+
         self.tree[item] = item
     
     def remove_point(self, item: T):
+        """
+        complexity best: O(CompK) delete the item at the root.
+        complexity worst: O(Compk * D) delete at the bottom of the tree
+        where D is the depth of the tree
+        CompK is the complexity of comparing the keys
+        Refer to __delitem__() in bst.BinarySearchTree
+        """
         del self.tree[item]
 
     def ratio(self, x, y):
+        """
+        complexity best: O(D)
+        complexity worst: O(D + R)
+        Refer to get_range() in bst.BinarySearchTree
+        """
         
         # converting percentage into start and end of the values at range
         a = ceil(x/100* len(self.tree)) + 1
